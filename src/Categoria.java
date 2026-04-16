31226c82cd9057e2a206ee8dc5fc649ca1b0f4a5
2222000313 <matheuswendt1@gmail.com>
2026-04-15 22:01:24 -0300

    aula_04

--- src/Categoria.java
+++ src/Categoria.java
@@ -9,6 +9,10 @@ public class Categoria {
     public String getNomeCategoria() {
         return nomeCategoria;
     }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
 }
 
 
